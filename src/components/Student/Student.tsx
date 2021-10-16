97f629fab17f55f6e0e0210b62cc6b18dd9aca14
Daniel Sottile <danielsottile@gmail.com>
2021-10-16 13:13:35 -0400

    added styles for students, part 2

--- src/components/Student/Student.tsx
+++ src/components/Student/Student.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react';
 import {average} from '../../utils/functions';
 import {StudentInformation} from '../../@types';
+import styles from './Student.module.css';
 
 type StudentProps = {
   /** id for folder*/
@@ -14,14 +15,20 @@ const Student: FunctionComponent<StudentProps> = ({ info }) => {
   const averageGrade = average(grades.map(grade => Number(grade)));
 
   return (
-    <>
-      <img src={pic} alt={`student ${firstName} ${lastName}`}></img>
-      <h2>{`${firstName} ${lastName}`}</h2>
-      <p>{`email: ${email}`}</p>
-      <p>{`Company: ${company}`}</p>
-      <p>{`Skill: ${skill}`}</p>
-      <p>{`Average: ${averageGrade}`}</p>
-    </>
+    <div className={styles['student']}>
+      <img className={styles['icon']} src={pic} alt={`student ${firstName} ${lastName}`}></img>
+      <div className={styles['student-info']}>
+        <h2>{`${firstName} ${lastName}`}</h2>
+        <div className={styles['information']}>
+          <p>{`email: ${email}`}</p>
+          <p>{`Company: ${company}`}</p>
+          <p>{`Skill: ${skill}`}</p>
+          <p>{`Average: ${averageGrade}`}</p>
+        </div>
+        
+      </div>
+      
+    </div>
   );
 };
 
--- src/components/StudentList/StudentList.tsx
+++ src/components/StudentList/StudentList.tsx
@@ -1,12 +1,13 @@
 import { useContext, FunctionComponent } from 'react';
 import DataContext from '../../contexts/context';
 import { StudentInformation } from '../../@types';
+import styles from './StudentList.module.css';
 import Student from '../Student';
 
 const StudentList: FunctionComponent = () => {
   const { data } = useContext(DataContext);
   const studentList = data.map((info: StudentInformation, index) => <Student key={index} info={info} />);
-  return <>{studentList}</>;
+  return <div className={styles['student-list']}>{studentList}</div>;
 };
 
 export default StudentList;
