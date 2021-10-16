52212107ea4bd169b549729eb3e99e382a9c0e23
Daniel Sottile <danielsottile@gmail.com>
2021-10-16 13:15:30 -0400

    removed uneccessary bits

--- src/components/StudentList/StudentList.tsx
+++ src/components/StudentList/StudentList.tsx
@@ -1,13 +1,12 @@
 import { useContext, FunctionComponent } from 'react';
 import DataContext from '../../contexts/context';
 import { StudentInformation } from '../../@types';
-import styles from './StudentList.module.css';
 import Student from '../Student';
 
 const StudentList: FunctionComponent = () => {
   const { data } = useContext(DataContext);
   const studentList = data.map((info: StudentInformation, index) => <Student key={index} info={info} />);
-  return <div className={styles['student-list']}>{studentList}</div>;
+  return <>{studentList}</>;
 };
 
 export default StudentList;
